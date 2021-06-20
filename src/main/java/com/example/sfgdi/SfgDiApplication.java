7dc275831243898b948373251103fc95c40e7d03
srijibbose <srijibbose.gmail.com>
2021-06-20 18:00:49 +0530

    Property Injected Controller2

--- src/main/java/com/example/sfgdi/SfgDiApplication.java
+++ src/main/java/com/example/sfgdi/SfgDiApplication.java
@@ -1,6 +1,7 @@
 package com.example.sfgdi;
 
 import com.example.sfgdi.controllers.MyController;
+import com.example.sfgdi.controllers.PropertyInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -17,6 +18,10 @@ public class SfgDiApplication {
         String greeting = myController.sayHello();
 
         System.out.println(greeting);
+
+        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+
+        System.out.println(propertyInjectedController.getGreeting());
     }
 
 }
